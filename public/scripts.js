14c17130dd48ae655b0c6e535bb7714dc86f864f
Something <rodrigo.pc.12@gmail.com>
2020-03-24 21:18:18 -0300

    Add NightMode

--- public/scripts.js
+++ public/scripts.js
@@ -4,3 +4,23 @@ const form = document.querySelector('.form');
 button.addEventListener("click", () => {
   form.classList.toggle('hide');
 });
+
+// NIGHT MODE \\
+
+const nightModeStorage = localStorage.getItem('NightMode')
+const nightMode = document.querySelector('#night-mode')
+
+if (nightModeStorage) {
+  document.documentElement.classList.add('night-mode')
+  nightMode.checked = true
+}
+
+nightMode.addEventListener('click', () => {
+  document.documentElement.classList.toggle('night-mode')
+  
+  if (document.documentElement.classList.contains('night-mode')) {
+    localStorage.setItem('NightMode', true)
+    return
+  }
+  localStorage.removeItem('NightMode')
+});
